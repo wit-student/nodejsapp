bde39a0e73263161e9c30b4f7b31b98461fc0701
Sakshi Kapse <149087233+wit-student@users.noreply.github.com>
2025-10-28 11:20:51 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
+console.log(" Starting app.js ...");
+
 const express = require("express");
+console.log("Express module loaded successfully.");
+
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello from Node.js CI/CD Pipeline 🚀");
+  res.send("Hello from Node.js CI/CD Pipeline ");
 });
 
+
+
 const port = process.env.PORT || 3000;
-app.listen(port, () => console.log(`Server running on port ${port}`));
+app.listen(port, () => {
+  console.log(` Server running on port ${port}`);
+});
+
